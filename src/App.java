9d3cba2332d6008e90f803479c47a8b74514d271
Megyeri <megyko1234@gmail.com>
2022-09-19 10:40:39 +0200

    Lista nevek kiírása

--- src/App.java
+++ src/App.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String[] nevek={"Pali","Dani", "Jani", "Milán","Levi"};
+        ArrayList<String>nevList= new ArrayList<String>(Arrays.asList(nevek));
+    
+        System.out.println(nevList.get(0));
+    }
+    
     }
 
 
-    String[] nevek=['Pali',"Dani", "Jani", "Milán","Levi"]
-    ArrayList<String>nevList= new ArrayList<String>(Arrays.asList(nevek));
-}
+ 
