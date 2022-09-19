ea986148fc7fa10730bec59739c405e89629857f
Megyeri <megyko1234@gmail.com>
2022-09-19 10:37:40 +0200

    ArrayList létrehozása

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+
+    String[] nevek=['Pali',"Dani", "Jani", "Milán","Levi"]
+    ArrayList<String>nevList= new ArrayList<String>(Arrays.asList(nevek));
 }
